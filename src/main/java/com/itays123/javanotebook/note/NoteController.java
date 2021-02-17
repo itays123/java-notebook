24c2fd4b1faeb8d4f98515311abc32f76b770110
itays123 <itays2005@gmail.com>
2021-02-17 15:54:11 +0200

    FEATURE: Get notes from database

---
+++ src/main/java/com/itays123/javanotebook/note/NoteController.java
@@ -0,0 +1,25 @@
+package com.itays123.javanotebook.note;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/note")
+public class NoteController {
+
+    private final NoteService noteService;
+
+    @Autowired
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping
+    public List<Note> getNotes() {
+        return noteService.getNotes();
+    }
+}
---
+++ src/main/java/com/itays123/javanotebook/note/NoteService.java
@@ -0,0 +1,21 @@
+package com.itays123.javanotebook.note;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteService {
+
+    private final NoteRepository noteRepository;
+
+    @Autowired
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    public List<Note> getNotes() {
+        return noteRepository.findAll();
+    }
+}
