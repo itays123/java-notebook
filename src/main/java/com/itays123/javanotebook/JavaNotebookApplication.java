ac7c228e705d353470025f296acac268209f4c58
itays123 <itays2005@gmail.com>
2021-02-17 15:21:28 +0200

    REFACTOR: Note & Block JPA logic

--- src/main/java/com/itays123/javanotebook/JavaNotebookApplication.java
+++ src/main/java/com/itays123/javanotebook/JavaNotebookApplication.java
@@ -1,13 +1,37 @@
 package com.itays123.javanotebook;
 
+import com.itays123.javanotebook.block.Block;
+import com.itays123.javanotebook.block.BlockType;
+import com.itays123.javanotebook.note.Note;
+import com.itays123.javanotebook.note.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class JavaNotebookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaNotebookApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(NoteRepository noteRepository) {
+		return args -> {
+			Note note = new Note("My First Note");
+
+			Block block1 = new Block("A Paragraph", BlockType.P);
+			Block block2 = new Block("A Heading", BlockType.H3);
+
+			note.getContent().add(block1);
+			note.getContent().add(block2);
+
+			noteRepository.save(note);
+		};
 	}
 
 }
---
+++ src/main/java/com/itays123/javanotebook/note/Note.java
@@ -0,0 +1,59 @@
+package com.itays123.javanotebook.note;
+
+import com.itays123.javanotebook.block.Block;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String title;
+
+    @OneToMany(targetEntity = Block.class, cascade = CascadeType.ALL)
+    @JoinColumn(name="note_id", referencedColumnName = "id")
+    private List<Block> content = new ArrayList<>();
+
+    public Note() {
+    }
+
+    public Note(String title) {
+        this.title = title;
+    }
+
+    public Note(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Block> getContent() {
+        return content;
+    }
+
+    public void setContent(List<Block> content) {
+        this.content = content;
+    }
+}
