53a9a82b752849dc0beb356f74d552f964844206
itays123 <itays2005@gmail.com>
2021-02-24 22:16:01 +0200

    FEATURE: Creamy theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cream-light': '#fffffb',
+        'cream': '#d7ccc8',
+        'cream-dark': '#a69b97',
+        'cream-darker': '#776d69'
+      }
+    },
   },
   variants: {
     extend: {},
