5eebefc029d9a5491dc718cfdabab6ce6e01cfbf
itays123 <itays2005@gmail.com>
2021-02-24 22:16:24 +0200

    FEATURE: Basic header

---
+++ frontend/src/shared/Navigation/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="w-full h-full bg-cream">
+            <div className="container mx-auto flex items-center h-full">
+                <div className="flex-grow logo text-2xl font-bold text-gray-800">
+                    Java Notebook
+                </div>
+                <div className="links"></div>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
--- frontend/src/shared/Navigation/NavigationWrapper.js
+++ frontend/src/shared/Navigation/NavigationWrapper.js
@@ -1,8 +1,12 @@
+import Navbar from "./Navbar"
+
 const NavigationWrapper = ({ children }) => {
     return ( 
         <div className="app flex flex-col overflow-y-hidden h-screen">
-     <div className="header-wrapper h-16"></div>
-     <div className="content-wrapper flex-grow relative">
+     <div className="header-wrapper h-16">
+         <Navbar />
+     </div>
+     <div className="content-wrapper flex-grow relative bg-cream-light">
          { children }
      </div>
      </div>
