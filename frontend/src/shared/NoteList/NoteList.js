36b32fc6cce819b73e4d09d08ce53689748af203
itays123 <itays2005@gmail.com>
2021-02-26 20:41:32 +0200

    FIX: Unique key for NoteItem

--- frontend/src/shared/NoteList/NoteList.js
+++ frontend/src/shared/NoteList/NoteList.js
@@ -3,7 +3,7 @@ import NoteItem from "./NoteItem";
 const NoteList = ({notes}) => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-            { notes.map(note => <NoteItem {...note} />) }
+            { notes.map(note => <NoteItem {...note} key={note.id} />) }
         </div>
     );
 }
